b541df6b4ab886c0baeac176925c5af5920bac4c
MouradBkarim <mouradkarim@gmail.com>
2015-12-24 14:33:06 +0200

    Remove test1

--- cherrypy/test1.py
+++
@@ -1,9 +0,0 @@
-import cherrypy
-
-class ChPy_server(object):
-    @cherrypy.expose
-    def index(self):
-        return "Hello world!"
-        
-if __name__ == '__main__':
-    cherrypy.quickstart(ChPy_server())
